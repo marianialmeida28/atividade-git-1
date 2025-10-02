dcb4b4976d77d38015ef26485e623a5c0373a570
marianialmeida28 <marianialmeida00@gmail.com>
2025-10-02 11:04:05 -0400

    cria data hora

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function mostrarMensagem() {
 function mudarCorDeFundo() {
   document.body.style.backgroundColor = "#e0f7fa";
 }
+function mostrarDataHora() {
+  const agora = new Date();
+  alert("Data e hora atual: " + agora.toLocaleString());
+}
